c5cd4a958eaec18c6de606dbde9fae97ec79d8ca
i23SebasRam <sebastianbeto2015@gmail.com>
2021-10-23 23:57:56 -0500

    new network

--- vision_python_repos/Segmentation/Resnet50.py
+++ vision_python_repos/Segmentation/Resnet50.py
@@ -11,7 +11,6 @@ from tensorflow.keras.utils import Sequence
 from tensorflow.keras.preprocessing.image import load_img
 import random 
 
-
 #Paths
 Path_img = "C:/Users/SEBASTIAN/OneDrive - Universidad de los Andes/Documentos/img_aug/x_aug/"
 Path_mask = "C:/Users/SEBASTIAN/OneDrive - Universidad de los Andes/Documentos/img_aug/y_aug/"
@@ -34,7 +33,6 @@ mask_paths = [
  #       os.path.join(Path_Train_Frames, fname)
   #      for fname in os.listdir(Path_Train_Frames)
    # ]
-
 
 #Split images
 cant_imagenes = 20000 #Cuantas imagenes tenemos.
@@ -66,15 +64,16 @@ class images(Sequence):
 
         x = np.zeros((self.batch_size,) + self.img_size + (3,), dtype = "float32")
         for j, path in enumerate(batch_input_img_paths):
-            img = load_img(path, target_size=self.img_size)
-            mx = np.max(img)
-            img = (img/mx) + 1 #Normalize the images.
+            img = load_img(path)
+            img = np.array(img,dtype='float32')
+            img = img/255
             x[j] = img
         y = np.zeros((self.batch_size,) + self.img_size + (1,), dtype = "uint8")
         for j, path in enumerate(batch_mask_img_paths):
             img = load_img(path, target_size=self.img_size,color_mode="grayscale")
+            img = np.array(img,dtype='uint8')
+            print(type(img))
             y[j] = np.expand_dims(img,2)
-        
         return x,y
 
 #Parameters
@@ -88,6 +87,7 @@ img_size = (480,640)
 base_model = ResNet50V2(weights='imagenet',
                         include_top=False,
                         input_shape=(480,640,3))
+
 base_model.trainable = False
 
 #Callbacks
@@ -110,24 +110,22 @@ for filters in [2048,1024,512,256,128,64]:
     if filters != 64:
         x = layers.UpSampling2D(2)(x)
 
-
-output = layers.Conv2D(9,3,activation="softmax",padding="same")(x)
+output = layers.Conv2D(7,3,activation="softmax",padding="same")(x)
 
 model = keras.Model(input,output)
 
 model.summary()
+
+
 
 #Batch organized images
 train_gen = images(Batch_Size, img_size, train_img_paths, train_mask_paths)
 val_gen = images(Batch_Size_val, img_size, val_img_paths, val_mask_paths)
 
 #Model compile and fit
-model.compile(
-    optimizer = 'SGD',
-    loss = 'sparse_categorical_crossentropy',
-    metrics = [keras.metrics.SparseCategoricalCrossentropy()]
-)
+opt = keras.optimizers.SGD(learning_rate=1e-6,momentum=0.9,clipvalue=1.0)
+loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+model.compile(optimizer = opt,loss = loss,metrics = [keras.metrics.SparseCategoricalCrossentropy()])
 
 model.fit(train_gen,epochs=No_Epochs,validation_data=val_gen, callbacks=callbacks_list)
-
-model.predict()
