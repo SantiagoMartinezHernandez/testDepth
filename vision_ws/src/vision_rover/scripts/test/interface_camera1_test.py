ecddcbb0fbb110b2a32e4365a0e3d44e0e9ae4c2
Cristobal Arroyo <j.arroyo@uniandes.edu.co>
2021-10-26 16:42:46 -0500

    Update numpy test al recbir los datos

--- vision_ws/src/vision_rover/scripts/test/interface_camera1_test.py
+++ vision_ws/src/vision_rover/scripts/test/interface_camera1_test.py
@@ -1,9 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float32
-from cv_bridge import CvBridge
+# from cv_bridge import CvBridge
 import cv2
+
+import numpy as np
+
+global bandera
+
+bandera = True
 
 # CONSTANTS PARAMETERS FOR CAMERAS BELOW
 CAMERA_NAME = '/cam1'
@@ -18,7 +24,26 @@ def start_node():
 
 def process_image(msg):
     global pub
-    showImage(CvBridge().imgmsg_to_cv2(msg))
+    global bandera
+
+    # img = CvBridge().imgmsg_to_cv2(msg)
+    img = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+
+    # if bandera: 
+    #     bandera = not bandera
+    #     print(type(msg))
+    #     print("-----------------------")
+    #     print(type(img))
+    #     print("-----------------------")
+    #     print(img.__repr__())
+    #     print("-----------------------")
+    #     print(img)
+
+    showImage(img)
+
+    # file = open("msgs.txt", 'w')
+    # file.write(str(img)+"\n")
+    # file.close()
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
